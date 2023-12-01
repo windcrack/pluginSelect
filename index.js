b4301a936e122867fc0e15b3ff9ee909c17ff62a
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2023-12-01 16:18:01 +0300

    fix

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import { Select } from './select/select.js'
-import './select/styles.scss'
+import { Select } from "./select/select.js";
+//import "./select/styles.css";
 
-const select = new Select('#select', {
-    placeholder: 'Выбери, пожалуйста, элемент',
-    selecetedId: '4',
-    data: [
-        {id: '1', value: 'React'},
-        {id: '2', value: 'Angular'},
-        {id: '3', value: 'Vue'},
-        {id: '4', value: 'React Native'},
-        {id: '5', value: 'Next'},
-        {id: '6', value: 'Nest'}
-    ],
-    onSelect(item){
-        console.log('Select item -> ', item)
-    }
-})
+const select = new Select("#select", {
+  placeholder: "Выбери, пожалуйста, элемент",
+  selecetedId: "4",
+  data: [
+    { id: "1", value: "React" },
+    { id: "2", value: "Angular" },
+    { id: "3", value: "Vue" },
+    { id: "4", value: "React Native" },
+    { id: "5", value: "Next" },
+    { id: "6", value: "Nest" },
+  ],
+  onSelect(item) {
+    console.log("Select item -> ", item);
+  },
+});
 
-window.s = select
+window.s = select;
